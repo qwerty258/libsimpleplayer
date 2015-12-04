bf3d3a7b8789222ebbc4bd8fce01110b258ab032
qwerty258 <zyf26256@gmail.com>
2015-12-04 16:38:05 +0800

    add cstring to char

---
+++ MSVS/VS2013/testPlayer/CStringToChar.h
@@ -0,0 +1,12 @@
+#pragma once
+class CCStringToChar
+{
+public:
+    CCStringToChar(void);
+    CCStringToChar(CString& cstring);
+    ~CCStringToChar(void);
+    char* GetCStyleString(void);
+private:
+    char* m_buffer;
+    int m_result;
+};
